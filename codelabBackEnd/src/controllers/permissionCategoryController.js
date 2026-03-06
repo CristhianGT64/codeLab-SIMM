6d200656f702dcebe5d2dced95270383f06272f7
Jhonnxh <jhonny.hernandez1807@gmail.com>
2026-03-06 11:22:50 -0600

    Improved controller response

--- codelabBackEnd/src/controllers/permissionCategoryController.js
+++ codelabBackEnd/src/controllers/permissionCategoryController.js
@@ -6,11 +6,17 @@ export const create = async (req, res) => {
 
     const category = await service.createPermissionCategory(req.body);
 
-    res.json(category);
+    res.json({
+      success: true,
+      data: category
+    });
 
   } catch (error) {
 
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
 
   }
 
@@ -22,11 +28,17 @@ export const getAll = async (req, res) => {
 
     const categories = await service.getAllPermissionCategories();
 
-    res.json(categories);
+    res.json({
+      success: true,
+      data: categories
+    });
 
   } catch (error) {
 
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
 
   }
 
