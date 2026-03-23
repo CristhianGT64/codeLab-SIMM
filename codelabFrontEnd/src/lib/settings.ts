e49dd26f3e3e855ac59a522374bb17037473e3ce
Cristhian Lopez <143055680+CristhianGT64@users.noreply.github.com>
2026-03-22 22:15:18 -0600

    Update settings.ts
    CAMBIOS

--- codelabFrontEnd/src/lib/settings.ts
+++ codelabFrontEnd/src/lib/settings.ts
@@ -1,8 +1,8 @@
 export default {
     // URL: "https://api-pokequequepcaiii-dev.azurewebsites.net",
-    //URL : "http://localhost:3000", // Para consumir api local
+    URL : "http://localhost:3000", // Para consumir api local
     prueba : "https://pokeapi.co/api/v2", // Para pruebas locales
-    URL : "https://api-codelab-unah.ambitiouswave-de8b838a.southcentralus.azurecontainerapps.io", // Consumir api de db actual
+    // URL : "https://api-codelab-unah.ambitiouswave-de8b838a.southcentralus.azurecontainerapps.io", // Consumir api de db actual
     year : new Date().getFullYear()
 };
 
