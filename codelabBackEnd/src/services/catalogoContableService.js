e3ca12949c2f867076e3809fa67c982d4646d296
SamuelPaz10 <111510648+SamuelPaz10@users.noreply.github.com>
2026-03-25 21:09:07 -0600

    Ajuste en codigo contable del catálogo

--- codelabBackEnd/src/services/catalogoContableService.js
+++ codelabBackEnd/src/services/catalogoContableService.js
@@ -1,43 +1,72 @@
 import catalogoContableRepository from '../repositories/catalogoContableRepository.js';
 
+function formatCodigo(num, size = 2) {
+  return String(num).padStart(size, '0');
+}
+
 const catalogoContableService = {
   async getArbol() {
     const { elementos, clasificaciones, cuentas, subcuentas } =
       await catalogoContableRepository.getAllData();
 
-    const arbol = elementos.map((elemento) => {
-      const clasificacionesElemento = clasificaciones
-        .filter(c => c.uuidElementoContable === elemento.uuidElementoContable)
-        .map((clasificacion) => {
+    const arbol = elementos
+      .sort((a, b) => a.codigoNumerico - b.codigoNumerico)
+      .map((elemento) => {
 
-          const cuentasClasificacion = cuentas
-            .filter(cu =>
-              cu.uuidElementoContable === elemento.uuidElementoContable &&
-              cu.uuidClasificacionContable === clasificacion.uuidClasificacionContable
-            )
-            .map((cuenta) => {
+        const codigoElemento = `${elemento.codigoNumerico}`;
 
-              const subcuentasCuenta = subcuentas.filter(sc =>
-                sc.uuidCuentaContable === cuenta.uuidCuentaContable
-              );
+        const clasificacionesElemento = clasificaciones
+          .filter(c => c.uuidElementoContable === elemento.uuidElementoContable)
+          .sort((a, b) => a.codigoNumerico - b.codigoNumerico)
+          .map((clasificacion) => {
 
-              return {
-                ...cuenta,
-                subcuentas: subcuentasCuenta,
-              };
-            });
+            const codigoClasificacion = `${codigoElemento}.${clasificacion.codigoNumerico}`;
 
-          return {
-            ...clasificacion,
-            cuentas: cuentasClasificacion,
-          };
-        });
+            const cuentasClasificacion = cuentas
+              .filter(cu =>
+                cu.uuidElementoContable === elemento.uuidElementoContable &&
+                cu.uuidClasificacionContable === clasificacion.uuidClasificacionContable
+              )
+              .sort((a, b) => a.codigoNumerico - b.codigoNumerico)
+              .map((cuenta) => {
 
-      return {
-        ...elemento,
-        clasificaciones: clasificacionesElemento,
-      };
-    });
+                const codigoCuenta =
+                  `${codigoClasificacion}.${formatCodigo(cuenta.codigoNumerico)}`;
+
+                const subcuentasCuenta = subcuentas
+                  .filter(sc => sc.uuidCuentaContable === cuenta.uuidCuentaContable)
+                  .sort((a, b) => a.codigoNumerico - b.codigoNumerico)
+                  .map((subcuenta) => {
+
+                    const codigoSubcuenta =
+                      `${codigoCuenta}.${formatCodigo(subcuenta.codigoNumerico, 3)}`;
+
+                    return {
+                      ...subcuenta,
+                      codigoContable: codigoSubcuenta,
+                    };
+                  });
+
+                return {
+                  ...cuenta,
+                  codigoContable: codigoCuenta,
+                  subcuentas: subcuentasCuenta,
+                };
+              });
+
+            return {
+              ...clasificacion,
+              codigoContable: codigoClasificacion,
+              cuentas: cuentasClasificacion,
+            };
+          });
+
+        return {
+          ...elemento,
+          codigoContable: codigoElemento,
+          clasificaciones: clasificacionesElemento,
+        };
+      });
 
     return arbol;
   },
